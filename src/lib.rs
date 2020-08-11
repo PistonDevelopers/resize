45a1ed66d99e67dc8ceb8fef6d5d8aeaa6d28e89
Kornel <kornel@geekhood.net>
2020-08-11 22:21:54 +0100

    Move code to a submodule

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,9 @@
 use std::sync::Arc;
 use std::collections::HashMap;
 use std::f32;
-use std::mem;
+
+mod px;
+pub use px::*;
 
 /// Resizing type to use.
 pub enum Type {
@@ -154,164 +156,6 @@ pub mod Pixel {
     pub struct RGBA64;
 }
 
-/// See `Pixel`
-pub trait PixelFormat {
-    /// Array to hold temporary values.
-    type Accumulator: AsRef<[f32]> + AsMut<[f32]>;
-    /// Type of a Subpixel of each pixel (8 or 16 bits).
-    type Subpixel: Copy;
-
-    /// New empty Accumulator.
-    fn new_accum() -> Self::Accumulator;
-
-    /// Convert float to integer value in range appropriate for this pixel format.
-    fn into_subpixel(v: f32) -> Self::Subpixel;
-
-    /// Convert pixel component to float
-    fn from_subpixel(v: &Self::Subpixel) -> f32;
-
-    /// Size of one pixel in that format in bytes.
-    #[inline(always)]
-    fn get_size(&self) -> usize {
-        self.get_ncomponents() * mem::size_of::<Self::Subpixel>()
-    }
-
-    /// Return number of components of that format.
-    #[inline(always)]
-    fn get_ncomponents(&self) -> usize {
-        Self::new_accum().as_ref().len()
-    }
-}
-
-impl PixelFormat for Pixel::Gray8 {
-    type Accumulator = [f32; 1];
-    type Subpixel = u8;
-
-    #[must_use]
-    #[inline(always)]
-    fn new_accum() -> Self::Accumulator {
-        [0.0; 1]
-    }
-
-    #[must_use]
-    #[inline(always)]
-    fn into_subpixel(v: f32) -> Self::Subpixel {
-        Resizer::<Self>::pack_u8(v)
-    }
-
-    #[inline(always)]
-    fn from_subpixel(px: &Self::Subpixel) -> f32 {
-        *px as f32
-    }
-}
-
-impl PixelFormat for Pixel::Gray16 {
-    type Accumulator = [f32; 1];
-    type Subpixel = u16;
-
-    #[must_use]
-    #[inline(always)]
-    fn new_accum() -> Self::Accumulator {
-        [0.0; 1]
-    }
-
-    #[must_use]
-    #[inline(always)]
-    fn into_subpixel(v: f32) -> Self::Subpixel {
-        Resizer::<Self>::pack_u16(v)
-    }
-
-    #[inline(always)]
-    fn from_subpixel(px: &Self::Subpixel) -> f32 {
-        *px as f32
-    }
-}
-
-impl PixelFormat for Pixel::RGB24 {
-    type Accumulator = [f32; 3];
-    type Subpixel = u8;
-
-    #[must_use]
-    #[inline(always)]
-    fn new_accum() -> Self::Accumulator {
-        [0.0; 3]
-    }
-
-    #[must_use]
-    #[inline(always)]
-    fn into_subpixel(v: f32) -> Self::Subpixel {
-        Resizer::<Self>::pack_u8(v)
-    }
-
-    #[inline(always)]
-    fn from_subpixel(px: &Self::Subpixel) -> f32 {
-        *px as f32
-    }
-}
-impl PixelFormat for Pixel::RGBA {
-    type Accumulator = [f32; 4];
-    type Subpixel = u8;
-
-    #[must_use]
-    #[inline(always)]
-    fn new_accum() -> Self::Accumulator {
-        [0.0; 4]
-    }
-
-    #[must_use]
-    #[inline(always)]
-    fn into_subpixel(v: f32) -> Self::Subpixel {
-        Resizer::<Self>::pack_u8(v)
-    }
-
-    #[inline(always)]
-    fn from_subpixel(px: &Self::Subpixel) -> f32 {
-        *px as f32
-    }
-}
-impl PixelFormat for Pixel::RGB48 {
-    type Accumulator = [f32; 3];
-    type Subpixel = u16;
-
-    #[must_use]
-    #[inline(always)]
-    fn new_accum() -> Self::Accumulator {
-        [0.0; 3]
-    }
-
-    #[must_use]
-    #[inline(always)]
-    fn into_subpixel(v: f32) -> Self::Subpixel {
-        Resizer::<Self>::pack_u16(v)
-    }
-
-    #[inline(always)]
-    fn from_subpixel(px: &Self::Subpixel) -> f32 {
-        *px as f32
-    }
-}
-impl PixelFormat for Pixel::RGBA64 {
-    type Accumulator = [f32; 4];
-    type Subpixel = u16;
-
-    #[must_use]
-    #[inline(always)]
-    fn new_accum() -> Self::Accumulator {
-        [0.0; 4]
-    }
-
-    #[must_use]
-    #[inline(always)]
-    fn into_subpixel(v: f32) -> Self::Subpixel {
-        Resizer::<Self>::pack_u16(v)
-    }
-
-    #[inline(always)]
-    fn from_subpixel(px: &Self::Subpixel) -> f32 {
-        *px as f32
-    }
-}
-
 /// Resampler with preallocated buffers and coeffecients for the given
 /// dimensions and filter type.
 #[derive(Debug)]
@@ -399,28 +243,6 @@ impl<Pixel: PixelFormat> Resizer<Pixel> {
             min
         } else {
             input
-        }
-    }
-
-    #[inline]
-    fn pack_u8(v: f32) -> u8 {
-        if v > 255.0 {
-            255
-        } else if v < 0.0 {
-            0
-        } else {
-            v.round() as u8
-        }
-    }
-
-    #[inline]
-    fn pack_u16(v: f32) -> u16 {
-        if v > 65535.0 {
-            65535
-        } else if v < 0.0 {
-            0
-        } else {
-            v.round() as u16
         }
     }
 
